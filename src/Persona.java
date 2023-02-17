9fd5cae635641b68b62fe6d9606e66be29e72a67
MatteoVenturelli <venturelli.matteo@itiscannizzaro.edu.it>
2023-02-17 12:23:46 +0100

    Classe Persona

---
+++ src/Persona.java
@@ -0,0 +1,12 @@
+
+public abstract class Persona {
+	String Nome, Cognome;
+    public Persona(String nome, String cognome) 
+    {
+        Nome=nome;
+        Cognome=cognome;
+    }
+    
+     abstract String generalita();
+
+}
